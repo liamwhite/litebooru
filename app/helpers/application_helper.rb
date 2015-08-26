33c0cf2859368171e0b46e22772b92d12e9ef5f4
Liam P. White <inkscapebrony@gmail.com>
2015-08-26 21:09:30 +0000

    THIS IS GONNA BE A REALLY FANCY BOORU YOU KNOW

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def render_time
+    diff = ((Time.now - @start_time) * 1000.0).round(2)
+    diff < 1000 ? "(#{diff}ms)" : "(#{(diff/1000.0).round(2)}s)"
+  rescue
+    "(nil ms)"
+  end
 end
