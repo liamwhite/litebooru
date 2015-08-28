f70b56b80ca5988224226af971c10984a1b48c3b
Liam P. White <inkscapebrony@gmail.com>
2015-08-28 00:22:25 +0000

    more tagsinput stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,11 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(function() {
+  $('.fancy-tags').tagsInput();
+});
