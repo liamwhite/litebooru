221258761cbcc8192a82e864d4e1a3c86f3ac172
Liam P. White <inkscapebrony@gmail.com>
2015-08-29 21:21:25 +0000

    That should do the trick

--- app/assets/javascripts/dropdown.js
+++ app/assets/javascripts/dropdown.js
@@ -1,25 +1,27 @@
 (function() {
   $.fn.setupDropdowns = function() {
     var self = $(this);
+    var sw = self.hasClass('sw');
     // Hide the dropdown menu by default.
     self.find('.dropdown-menu').hide();
     self.find('.dropdown-icon').click(function() {
-      toggleState(self);
+      toggleState(self, sw);
       return false;
     });
   }
 
-  function toggleState(container) {
+  function toggleState(container, sw) {
     var $menu = $(container).find('.dropdown-menu');
     if ($menu.is(':visible')) {
       // Hide the menu.
       $menu.hide();
     } else {
-      // Determine the necessary width for the dropdown.
-      var width = Math.max.apply(Math, $menu.find('a').map(function(){ return $(this).width(); }).get());
       // Position the menu.
       var top = ($(container).offset().top+5+$(container).outerHeight())+"px";
-      $menu.css({'top':top, 'min-width':width});
+      $menu.css({'top':top});
+      if (sw) {
+        $menu.css({'right':'5px'});
+      }
       // Show the menu.
       $menu.show();
     }
