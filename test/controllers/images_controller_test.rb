c1d99e018132c4a3650ad859f2dc14a5eefdcfc4
Liam P. White <inkscapebrony@gmail.com>
2015-09-11 19:49:43 +0000

    Fix ImageController tests

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -1,13 +1,18 @@
 require 'test_helper'
+require 'rack/test'
 
 class ImagesControllerTest < ActionController::TestCase
+  setup do
+    @image = images(:zero)
+  end
+
   test "should get index" do
     get :index
     assert_response :success
   end
 
   test "should get show" do
-    get :show
+    get :show, id: @image
     assert_response :success
   end
 
@@ -16,9 +21,11 @@ class ImagesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get create" do
-    get :create
-    assert_response :success
+  test "should create image" do
+    image_file = Rack::Test::UploadedFile.new(Rails.root.join('test','fixtures','image_files','water_drop.png'), "image/png")
+    assert_difference('Image.count') do
+      post :create, image: { description: @image.description, source_url: @image.source_url, tag_list: 'safe,water,simple background', image: image_file }
+    end
+    assert_redirected_to image_path(assigns(:image))
   end
-
 end
